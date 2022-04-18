5c60b2bb2db50ef099646237f800ea662ebdac7b
songkyung <yysk_915@naver.com>
2022-04-18 14:03:54 +0900

    [Add] status, responseMessage 모듈화

--- seminar2/level2/src/api/user.ts
+++ seminar2/level2/src/api/user.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, Router } from 'express';
+import { sc, rm } from '../constant';
 
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => { 
     return res.status(200).json({
-        status: 200,
-        message: 'user 조회 성공'
+        status: sc.OK,
+        message: rm.READ_USER_SUCCESS
     });
 });
 
