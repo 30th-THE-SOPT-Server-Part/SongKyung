a4f277bbe42c6148e3b3efed232837e89de37f26
songkyung <yysk_915@naver.com>
2022-06-16 17:18:00 +0900

    [Add] 다중 파일 업로드

--- seminar7/level1,level2/src/controllers/FileController.ts
+++ seminar7/level1,level2/src/controllers/FileController.ts
@@ -4,6 +4,7 @@ import statusCode from "../modules/statusCode";
 import util from "../modules/util";
 import { FileService } from "../services";
 
+// 단일 파일
 // multer upload 미들웨어에서 s3에 저장된 파일 주소는 req.file.location에 위치함
 // 미들웨어 만든거 넣어주고, 파일 주소를 저장만 해주면 된다.
 const uploadFileToS3 = async (req: Request, res: Response) => {
@@ -21,6 +22,37 @@ const uploadFileToS3 = async (req: Request, res: Response) => {
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
     }
 }
+
+// 다중 파일
+
+const uploadFilesToS3 = async (req: Request, res: Response) => {
+    if (!req.files) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+
+    // 배열로 들어온다.
+    const images: Express.MulterS3.File[] = req.files as Express.MulterS3.File[];
+
+    try {
+        // 이미지 위치, 이름로 된 배열로 만들어주기. 
+        const imageList: {
+            location: string;
+            originalname: string;
+        }[] = await Promise.all(images.map((image: Express.MulterS3.File) => {
+            return {
+                location: image.location,
+                originalname: image.originalname
+            }
+        }));
+
+        const data = await FileService.createFiles(imageList);
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.CREATE_FILE_SUCCESS, data));
+
+     } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
+}
+
 export default{
     uploadFileToS3,
+    uploadFilesToS3,
 }
--- seminar7/level1,level2/src/routes/FileRouter.ts
+++ seminar7/level1,level2/src/routes/FileRouter.ts
@@ -4,6 +4,9 @@ import upload from "../config/multer";
 
 const router: Router = Router();
 
-router.post('/upload', upload.single('file'), FileController.uploadFileToS3);
+// router.post('/upload', upload.single('file'), FileController.uploadFileToS3);
+
+// 여러개를 위해 single대신 array로 사용
+router.post('/upload', upload.array('file'), FileController.uploadFilesToS3);
 
 export default router;
